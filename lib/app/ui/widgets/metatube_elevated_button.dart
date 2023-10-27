7fa38146a08fa42542cda90b775ae50c55fb6435
Manel <manuelss.23.bil@gmail.com>
2023-10-26 23:00:13 -0300

    fix(style): ElevatedButton as statefull widget

--- lib/app/ui/widgets/metatube_elevated_button.dart
+++ lib/app/ui/widgets/metatube_elevated_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:metatube/app/utils/app_theme.dart';
 
-class MetatubeElevatedButton extends StatelessWidget {
+class MetatubeElevatedButton extends StatefulWidget {
   final void Function()? onPressed;
   final Widget child;
 
@@ -12,16 +12,21 @@ class MetatubeElevatedButton extends StatelessWidget {
   });
 
   @override
+  State<MetatubeElevatedButton> createState() => _MetatubeElevatedButtonState();
+}
+
+class _MetatubeElevatedButtonState extends State<MetatubeElevatedButton> {
+  @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: widget.onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: AppTheme.accent,
         foregroundColor: AppTheme.dark,
         disabledBackgroundColor: AppTheme.disableBackgroundColor,
         disabledForegroundColor: AppTheme.disableForegroundColor,
       ),
-      child: child,
+      child: widget.child,
     );
   }
 }
