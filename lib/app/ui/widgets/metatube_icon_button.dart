7ab1865d01f3eccbc8749c0c175391d4c9af5db2
Manel <manuelss.23.bil@gmail.com>
2023-10-26 22:10:06 -0300

    fix(style): IconButton correction to accept another color if necessary

--- lib/app/ui/widgets/metatube_icon_button.dart
+++ lib/app/ui/widgets/metatube_icon_button.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:metatube/app/utils/app_theme.dart';
 
-class MetatubeIconButton extends StatelessWidget {
+class MetatubeIconButton extends StatefulWidget {
   final void Function()? onPressed;
   final Icon icon;
+  final Color? color;
 
   const MetatubeIconButton({
     super.key,
     required this.icon,
     required this.onPressed,
+    this.color,
   });
 
   @override
+  State<MetatubeIconButton> createState() => _MetatubeIconButtonState();
+}
+
+class _MetatubeIconButtonState extends State<MetatubeIconButton> {
+  @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: onPressed,
+      onPressed: widget.onPressed,
       splashRadius: 20,
       splashColor: AppTheme.accent,
-      color: AppTheme.medium,
-      icon: icon,
+      color: widget.color ?? AppTheme.medium,
+      icon: widget.icon,
     );
   }
 }
